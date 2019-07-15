1bd5ff8fe267b004de4faefda4841bfb7c745f95
Илья <idzhyan.develop@outlook.com>
2019-07-15 17:43:16 +0300

    Добавьте файлы проекта.

---
+++ identity.fitness-pro.ru/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace identity.fitness_pro.ru.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
