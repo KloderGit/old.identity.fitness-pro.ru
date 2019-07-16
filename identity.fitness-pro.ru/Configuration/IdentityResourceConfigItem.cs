6a2e606d7e6b09193e33b197317bb193deb87d7e
Илья <idzhyan.develop@outlook.com>
2019-07-16 11:13:07 +0300

    add symlink to local git project with config json files add setting field to startup

---
+++ identity.fitness-pro.ru/Configuration/IdentityResourceConfigItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace identity.fitness_pro.ru.Configuration
+{
+    public class IdentityResourceConfigItem
+    {
+        public string DisplayName { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<string> UserClaims { get; set; }
+    }
+}
--- identity.fitness-pro.ru/Startup.cs
+++ identity.fitness-pro.ru/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using identity.fitness_pro.ru.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -9,34 +10,68 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 namespace identity.fitness_pro.ru
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public IConfiguration Configuration { get; }
+        public IConfiguration Settings { get; }
+        public IHostingEnvironment Environment { get; }
+
+        public Startup(IConfiguration configuration, IHostingEnvironment environment)
         {
             Configuration = configuration;
+            Environment = environment;
+
+            var cc = Configuration.GetSection("SettingsFilePath").Value;
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(environment.ContentRootPath)
+                .AddJsonFile(Configuration.GetSection("SettingsFilePath").Value + @"\clients.json", true, true)
+                .AddJsonFile(Configuration.GetSection("SettingsFilePath").Value + @"\identityresources.json", true, true);
+            Settings = builder.Build();
+
+            //var builder = new ConfigurationBuilder()
+            //    .SetBasePath(environment.ContentRootPath)
+            //    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            //    .AddJsonFile($"appsettings.{environment.EnvironmentName}.json", optional: true)
+            //    .AddEnvironmentVariables()
+            //    .AddJsonFile(Configuration.GetSection("SettingsFilePath").Value + @"\clients.json", true, true);
+            //Configuration = builder.Build();
         }
 
-        public IConfiguration Configuration { get; }
+
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
+            //services.Configure<CookiePolicyOptions>(options =>
+            //{
+            //    // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+            //    options.CheckConsentNeeded = context => true;
+            //    options.MinimumSameSitePolicy = SameSiteMode.None;
+            //});
+
+
+
+
+
+
+            services.Configure<IEnumerable<IdentityResourceConfigItem>>(Settings.GetSection("CustomIdentityClaims"));
+
+
+
+
+
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IOptions<IdentityResourceConfigItem> options)
         {
             if (env.IsDevelopment())
             {
