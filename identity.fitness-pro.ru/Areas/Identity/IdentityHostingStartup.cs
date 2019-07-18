8d8220e6a20f91ed37faf8bca57777e062e11925
Илья <idzhyan.develop@outlook.com>
2019-07-18 18:04:21 +0300

    add Basic UI Pages to control Account

---
+++ identity.fitness-pro.ru/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using identity.fitness_pro.ru.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(identity.fitness_pro.ru.Areas.Identity.IdentityHostingStartup))]
+namespace identity.fitness_pro.ru.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
