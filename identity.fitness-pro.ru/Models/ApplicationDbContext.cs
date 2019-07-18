cf4120a9586f6ab73e527ec920ab771d3a2142d8
Илья <idzhyan.develop@outlook.com>
2019-07-18 17:33:10 +0300

    Add ApplicationUser ApplicationContext and migrations add AspIdentity

---
+++ identity.fitness-pro.ru/Models/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace identity.fitness_pro.ru.Models
+{
+    public class ApplicationContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
---
+++ identity.fitness-pro.ru/Models/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace identity.fitness_pro.ru.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string ExternalGuid1C { get; set; }
+        public DateTime Birthday { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+    }
+}
